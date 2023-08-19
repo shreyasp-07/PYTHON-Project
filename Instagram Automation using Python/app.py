0c99fa520607ed14bec93cadf8544125620cd66c
Shreyas <shreyaspachpute2002@gmail.com>
2023-08-19 20:14:49 +0530

    change in insta bot app

--- Instagram Automation using Python/app.py
+++ Instagram Automation using Python/app.py
@@ -1,14 +1,14 @@
 from instabot import Bot
 
 bot = Bot()
-# bot.login(username="wtf_jammess",password="")
-# bot.follow('wscubetechindia')
-# bot.upload_photo("--path--",caption="I love Python")
-# bot.unfollow("wscubetechindia")
-# bot.send_message("I love Python",["rv_goswami_2","wscubetechindia"])
-followers = bot.get_user_followers("luvely.mikaa")
+bot.login(username="",password="")
+bot.follow('wscubetechindia')
+bot.upload_photo("--path--",caption="I love Python")
+bot.unfollow("wscubetechindia")
+bot.send_message("I love Python",["rv_goswami_2","wscubetechindia"])
+followers = bot.get_user_followers("insta-id")
 for follower in followers:
     print(bot.get_user_info(follower))
-following = bot.get_user_following("luvely.mikaa")
+following = bot.get_user_following("insts-id")
 for follower in following:
     print(bot.get_user_info(follower))
